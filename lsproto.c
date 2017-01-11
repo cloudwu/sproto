e869372b6eaf240e4047354654a232ef0bd43cd6
thomasheep256 <thomasheep256@gmail.com>
2017-01-11 19:11:21 +0800

    Update lsproto.c

--- lsproto.c
+++ lsproto.c
@@ -673,11 +673,8 @@ ldefault(lua_State *L) {
 	return 1;
 }
 
-#ifdef _MSC_VER
-int __declspec(dllexport)
-#else
-int
-#endif
+
+int LUAMOD_API
 luaopen_sproto_core(lua_State *L) {
 #ifdef luaL_checkversion
 	luaL_checkversion(L);
