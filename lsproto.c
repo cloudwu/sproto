fe78f5cc81eb42bd731a6b0f3921c9686f641c6a
Stocom <zpyang@stocom.net>
2017-01-12 09:11:47 +0800

    Revert "Update lsproto.c"
    This reverts commit 1daf94fef499c53ce3ca85b08dd76976cc1d620e.

--- lsproto.c
+++ lsproto.c
@@ -673,11 +673,7 @@ ldefault(lua_State *L) {
 	return 1;
 }
 
-#ifdef _MSC_VER
-int __declspec(dllexport)
-#else
 int
-#endif
 luaopen_sproto_core(lua_State *L) {
 #ifdef luaL_checkversion
 	luaL_checkversion(L);
